2600524e588442a88a19294dd49f4c797fbbe1d6
Mads Nielsen <madscwn@gmail.com>
2022-05-02 20:38:51 +0200

    add zsh extensions and y input to starship

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -5,7 +5,10 @@ sudo apt install zsh exa -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 cp .zshrc ~/
 
-curl -sS https://starship.rs/install.sh | sh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+y | curl -sS https://starship.rs/install.sh | sh
 mkdir -p ~/.config
 cp starship.toml ~/.config/
 
