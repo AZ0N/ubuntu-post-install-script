ae49da7eca719a7bd6fdc100a03f3577115c6b95
Mads Nielsen <madscwn@gmail.com>
2022-05-02 20:38:10 +0200

    add -y to automatically remove snapd

--- remove-snapd.sh
+++ remove-snapd.sh
@@ -2,6 +2,6 @@
 
 sudo snap remove --purge firefox
 sudo rm -rf /var/cache/snapd/
-sudo apt autoremove --purge snapd gnome-software-plugin-snap
+sudo apt autoremove --purge snapd gnome-software-plugin-snap -y
 rm -fr ~/snap
 sudo apt-mark hold snapd
